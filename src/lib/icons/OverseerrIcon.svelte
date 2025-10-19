bdc6b120ea1898be0b5233437cd0d6468782d0cf
Wryscher72 <gneupert@gmail.com>
2025-10-19 16:38:23 -0400

    Refactor OverseerrIcon component properties

--- src/lib/icons/OverseerrIcon.svelte
+++ src/lib/icons/OverseerrIcon.svelte
@@ -1,18 +1,8 @@
-<script lang="ts">
-  export let size = 18;
-  export let className = '';
-</script>
-
 <svg
-  class={className}
-  width={size}
-  height={size}
-  viewBox="0 0 24 24"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
-  aria-hidden="true"
-  focusable="false"
->
+  class="ico over"
+  width="18" height="18"
+  viewBox="0 0 24 24" fill="none"
+  xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
   <circle cx="12" cy="12" r="9" stroke="currentColor" stroke-width="2"/>
   <circle cx="12" cy="12" r="4" stroke="currentColor" stroke-width="2"/>
 </svg>
